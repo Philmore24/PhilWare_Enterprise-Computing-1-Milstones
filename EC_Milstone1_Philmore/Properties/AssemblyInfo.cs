ccfd3014420faab335d5214720fc2a096b166948
Philmore Foster <fosterphilmore@outlook.com>
2021-09-26 09:01:10 -0500

    just the assembly info of my project information

--- EC_Milstone1_Philmore/Properties/AssemblyInfo.cs
+++ EC_Milstone1_Philmore/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EC_Milstone1_Philmore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is just my enterprise computing 1 milstones")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EC_Milstone1_Philmore")]
